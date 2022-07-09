5a6ae7755b66c91785d5770ce1a9c96ed68fe1d8
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-09 18:13:19 -0500

    paginacion

--- src/app/PokemonApp.jsx
+++ src/app/PokemonApp.jsx
@@ -1,27 +1,34 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { startLoadingPokemons, setPokemons, getPokemos } from "../index";
+import { getPokemos } from "../index";
 
 export const PokemonApp = () => {
-  const { pokemons } = useSelector((state) => state.pokemon);
+  const { pokemons, isLoading, pages } = useSelector((state) => state.pokemon);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getPokemos());
   }, []);
-  console.log(pokemons);
+
   return (
     <>
       <h1>Pokemon App</h1>
       <hr />
+      {isLoading ? (
+        <span>Cargando...</span>
+      ) : (
+        <ul>
+          {pokemons.map(({ name }) => (
+            <li key={name}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      )}
 
-      <ul>
-        {pokemons.map((pokemon) => (
-          <li key={pokemon.url}>
-            <p>{pokemon.name}</p>
-          </li>
-        ))}
-      </ul>
+      <button disabled={isLoading} onClick={() => dispatch(getPokemos(pages))}>
+        Next
+      </button>
     </>
   );
 };
