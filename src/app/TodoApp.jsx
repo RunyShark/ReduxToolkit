42a0e042a9f13578f7036cd3b47e584709a1b770
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-09 19:16:18 -0500

    RTK

--- src/app/TodoApp.jsx
+++ src/app/TodoApp.jsx
@@ -1,11 +1,30 @@
+import { useGetTodosQuery } from "../index";
+
 export const TodoApp = () => {
+  const { data: todos = [], isLoading } = useGetTodosQuery();
+
   return (
     <>
       <h1>Todos - RKT Query</h1>
       <hr />
-      <h4>isLoading...</h4>
+      <h4>isLoading.: {isLoading ? "true" : "false"}</h4>
 
       <pre>...</pre>
+      <ul>
+        {todos.map(({ id, title, completed }) => (
+          <li key={id}>
+            <p>{title}</p>
+            <p style={{ fontFamily: "unset" }}>
+              Completed:{" "}
+              {completed ? (
+                <strong style={{ color: "green" }}>DONE</strong>
+              ) : (
+                <strong style={{ color: "red" }}>Pending</strong>
+              )}
+            </p>
+          </li>
+        ))}
+      </ul>
       <button>Next todo</button>
     </>
   );
